af35b85aef085e349514800bb6a1bfead29c9aea
Rafe Goldberg <rafegoldberg@gmail.com>
2022-02-20 13:40:53 -0800

    fix config to env mappings

--- server/db.mjs
+++ server/db.mjs
@@ -5,8 +5,6 @@ mongo.connection.on("error", (err) => console.error(err));
 // mongo.connection.on("connected", () => console.log("DB Connected"));
 // mongo.connection.on("disconnected", () => console.log("DB Disconnected"));
 
-const { DB } = process.env;
-
 const disconnect = mongo.disconnect;
 const connect = () =>
   mongo.connect(config.mongo.uri, {
